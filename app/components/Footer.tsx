5734be99db29a44f8a6463560638d4dfb0f73dd7
Ross Cohen <rosscohen1990@gmail.com>
2024-11-16 18:06:39 -0500

    u

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
-import TwitterSVG from "../../public/twitter.svg";
-import GithubSVG from "../../public/Github.svg";
-import Image from "next/image";
+// import TwitterSVG from "../../public/twitter.svg";
+// import GithubSVG from "../../public/Github.svg";
+// import Image from "next/image";
 
 const Footer = () => (
   <div className="flex flex-col items-center justify-between bg-[#343434] px-4 py-4 md:flex-row">
