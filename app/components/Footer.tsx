db35276f0202e7c89fe24ea78ff788c9b29a4581
Ross Cohen <rosscohen1990@gmail.com>
2024-11-16 18:02:42 -0500

    u

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,30 +7,30 @@ const Footer = () => (
     <div className="mb-2 flex-grow pl-0 text-center md:mb-0 md:pl-0 lg:pl-48">
       <span className="text-sm text-[#6F6F6F]">
         Powered by{" "}
-        <a href="https://dub.sh/together-ai" className="underline">
-          Together.ai
+        <a href="https://pixo.myapps.ai" className="underline">
+          Pixio
         </a>{" "}
         &{" "}
-        <a href="https://dub.sh/flux-playground" className="underline">
-          Flux
+        <a href="https://myapps.ai" className="underline">
+          AI Tutor
         </a>
       </span>
     </div>
     <div className="mb-2 flex space-x-3 md:mb-0">
-      <a
+      {/* <a
         href="https://github.com/Nutlope/logocreator"
         className="flex items-center rounded border border-[#6F6F6F] px-2 py-1 text-sm text-[#6F6F6F]"
       >
         <Image src={GithubSVG} alt="Twitter" className="mr-1 h-5 w-5" />
         Github
-      </a>
-      <a
+      </a> */}
+      {/* <a
         href="https://x.com/nutlope"
         className="flex items-center rounded border border-[#6F6F6F] px-2 py-1 text-sm text-[#6F6F6F]"
       >
         <Image src={TwitterSVG} alt="github" className="mr-1 h-4 w-4" />
         Twitter
-      </a>
+      </a> */}
     </div>
   </div>
 );
